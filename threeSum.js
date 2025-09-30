f0fe7c6785b38e312d5839af5112044afa56f31e
Ian Isaacson <ian_isaacson@yahoo.com>
2025-09-29 18:01:05 -0700

    Add core logic, comments

--- threeSum.js
+++ threeSum.js
@@ -1,19 +1,43 @@
 function threeSum(nums) {
+  // Preprocessing: Sort the input array
   const numsLength = nums.length;
-  const seen = new Map();
+  nums = nums.sort((a, b) => a - b);
+  const zeroSumTriplets = [];
 
-  if (numsLength === 3) {
-    return [nums];
-  }
-  for (const num of nums) {
-    if (!seen.has(num)) {
-      seen.set(num, 1);
-    } else {
-      seen.set(num, seen.get(num) + 1);
+  // Pointer/Processing: Iterate and use two pointers to find triplets
+  for (let i = 0; i < numsLength; i++) {
+    if (nums[i] > 0) {
+      break; // No three positive numbers can sum to zero
+    }
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue; // Skip duplicate values for i
+    }
+    const num = nums[i];
+    let left = i + 1;
+    let right = numsLength - 1;
+
+    while (left < right) {
+      const sum = num + nums[left] + nums[right];
+      if (sum === 0) {
+        zeroSumTriplets.push([num, nums[left], nums[right]]);
+        left++;
+        right--;
+        // Postprocessing: Skip duplicate values for left and right pointers
+        while (left < right && nums[left] === nums[left - 1]) {
+          left++;
+        }
+        while (left < right && nums[right] === nums[right + 1]) {
+          right--;
+        }
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
     }
   }
-
-  console.log(seen);
+  // Postprocessing: Return all unique triplets
+  return zeroSumTriplets;
 }
 
-export { threeSum };
+//export { threeSum };
